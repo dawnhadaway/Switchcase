b611715632c02d8da03daf98e4f2fcf945f84d14
Dawn Hadaway <63307856+dawnhadaway@users.noreply.github.com>
2020-08-25 21:29:47 -0400

    Add project files.

---
+++ Switchcase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Switchcase
+{
+    class Program
+    {
+        public static object userInput { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject");
+            var userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "math":
+                    Console.WriteLine("Math is to hard");
+                    break;
+                case "reading":
+                    Console.WriteLine("reading can be fun");
+                    break;
+                default:
+                    Console.WriteLine("That sounds interesting");
+                    break;
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
